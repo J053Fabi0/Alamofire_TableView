cd2f1a071ab166f645b35d8f8a9d8cc4acbce55e
J053Fabi0 <arguellojf1@gmail.com>
2017-04-07 09:08:02 -0600

    Saving changes

---
+++ AlamofireTableView/CustomTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  CustomTableViewCell.swift
+//  AlamofireTableView
+//
+//  Created by Jose Fabio_ on 07/04/17.
+//  Copyright © 2017 com.yoyomero. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
