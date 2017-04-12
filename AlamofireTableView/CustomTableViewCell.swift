dd3a43447df96fa415ab1881e70f799fdb370b44
J053Fabi0 <arguellojf1@gmail.com>
2017-04-11 21:04:45 -0600

    First commit

--- AlamofireTableView/CustomTableViewCell.swift
+++ AlamofireTableView/CustomTableViewCell.swift
@@ -8,28 +8,20 @@
 
 import UIKit
 
-class CustomTableViewCell: UIViewController {
+class CustomTableViewCell: UITableViewCell {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var categoryLabel: UILabel!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
-    */
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
 
 }
--- AlamofireTableView/ViewController.swift
+++ AlamofireTableView/ViewController.swift
@@ -7,20 +7,45 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    var actorsArray = [AnyObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://thecodeeasy.com/test/swiftjson.json").responseJSON { response in
+            
+            let result = response.result
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                if let innerDict = dict["actors"]{
+                    self.actorsArray = innerDict as! [AnyObject]
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return actorsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
+        
+        let title = actorsArray[indexPath.row]["title"]
+        let category = actorsArray[indexPath.row]["category"]
+        cell.categoryLabel.text = category as? String
+        cell.titleLabel.text = title as? String
+        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBOutlet var talbeView: UITableView!
+    
 
 }
 
